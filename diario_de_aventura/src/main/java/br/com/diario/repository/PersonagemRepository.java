a14146a7440fbb24a1ddcfd492f53b65546b0632
ArthurGabrielFreitas <artgabrielfdm@gmail.com>
2025-12-09 23:32:32 -0300

    Adição de repository de personagem

---
+++ diario_de_aventura/src/main/java/br/com/diario/repository/PersonagemRepository.java
@@ -0,0 +1,10 @@
+package br.com.diarioaventuras.repository;
+
+import br.com.diarioaventuras.entity.Personagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonagemRepository extends JpaRepository<Personagem, Long> {
+}
+public class PersonagemRepository {
+
+}
