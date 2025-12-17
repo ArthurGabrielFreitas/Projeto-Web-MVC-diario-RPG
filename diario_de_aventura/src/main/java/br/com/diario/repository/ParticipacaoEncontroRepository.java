20daddbdc87d7f6b86fb68787930eacdff6f7978
ArthurGabrielFreitas <artgabrielfdm@gmail.com>
2025-12-17 00:21:59 -0300

    Adição de consultas SQL personalizadas

--- diario_de_aventura/src/main/java/br/com/diario/repository/ParticipacaoEncontroRepository.java
+++ diario_de_aventura/src/main/java/br/com/diario/repository/ParticipacaoEncontroRepository.java
@@ -1,9 +1,23 @@
 package br.com.diario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 import br.com.diario.model.ParticipacaoEncontro;
 
 public interface ParticipacaoEncontroRepository extends JpaRepository<ParticipacaoEncontro, Long> {
+
+	@Query("SELECT p.personagem.id, p.personagem.nome, COUNT(p) FROM ParticipacaoEncontro p WHERE p.personagem IS NOT NULL GROUP BY p.personagem.id, p.personagem.nome ORDER BY COUNT(p) DESC")
+	List<Object[]> countParticipacoesPorPersonagem();
+
+	@Query("SELECT p.ameaca.id, p.ameaca.nome, COUNT(p) FROM ParticipacaoEncontro p WHERE p.ameaca IS NOT NULL GROUP BY p.ameaca.id, p.ameaca.nome ORDER BY COUNT(p) DESC")
+	List<Object[]> countParticipacoesPorAmeaca();
+
+	@Query("SELECT p.personagem.id, p.personagem.nome, COUNT(p) FROM ParticipacaoEncontro p WHERE p.ultimoGolpe = true AND p.personagem IS NOT NULL GROUP BY p.personagem.id, p.personagem.nome ORDER BY COUNT(p) DESC")
+	List<Object[]> countUltimoGolpePorPersonagem();
+
+	@Query("SELECT p.ameaca.id, p.ameaca.nome, COUNT(p) FROM ParticipacaoEncontro p WHERE p.ultimoGolpe = true AND p.ameaca IS NOT NULL GROUP BY p.ameaca.id, p.ameaca.nome ORDER BY COUNT(p) DESC")
+	List<Object[]> countUltimoGolpePorAmeaca();
 
 }
--- diario_de_aventura/src/main/java/br/com/diario/repository/PersonagemRepository.java
+++ diario_de_aventura/src/main/java/br/com/diario/repository/PersonagemRepository.java
@@ -2,6 +2,16 @@ package br.com.diario.repository;
 
 import br.com.diario.model.Personagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PersonagemRepository extends JpaRepository<Personagem, Long> {
+
+	@Query("SELECT p.raca, COUNT(p) FROM Personagem p GROUP BY p.raca ORDER BY COUNT(p) DESC")
+	List<Object[]> countByRaca();
+
+	@Query("SELECT p.classe, COUNT(p) FROM Personagem p GROUP BY p.classe ORDER BY COUNT(p) DESC")
+	List<Object[]> countByClasse();
+
 }
